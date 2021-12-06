3b400b7c0221fe601225c0478943b2e22d8c30f8
parkeunsik <qkrdmstlr3@naver.com>
2021-12-07 00:15:32 +0900

    [chore] eslint 설정
    - eslint 관련 파일들 설치 및 .eslintrc파일 생성

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,29 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "portfolio",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'portfolio',
   },
   plugins: [
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     // {
-    //   resolve: "gatsby-plugin-manifest",
+    //   resolve: 'gatsby-plugin-manifest',
     //   options: {
-    //     icon: "src/images/icon.png",
+    //     icon: 'src/images/icon.png',
     //   },
     // },
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     // {
-    //   resolve: "gatsby-source-filesystem",
+    //   resolve: 'gatsby-source-filesystem',
     //   options: {
-    //     name: "images",
-    //     path: "./src/images/",
+    //     name: 'images',
+    //     path: './src/images/',
     //   },
-    //   __key: "images",
+    //   __key: 'images',
     // },
   ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import React from 'react';
 
-const IndexPage = () => {
+function IndexPage() {
   return <div>hello</div>;
-};
+}
 
 export default IndexPage;
