7720377dcef8d143697718d94c9e84d9c56ce90a
parkeunsik <qkrdmstlr3@naver.com>
2021-12-11 01:24:54 +0900

    [feat] useAudio hook 구현
    - 음악 재생을 위한 hook구현

---
+++ src/hooks/useAudio.ts
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+
+interface UseAudioType {
+  playing: boolean;
+  muted: boolean;
+  togglePlaying: () => void;
+  toggleMute: () => void;
+}
+
+const useAudio = (url: string): UseAudioType => {
+  const audio = useRef<HTMLAudioElement>(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+  const [muted, setMuted] = useState(false);
+
+  useEffect(() => {
+    audio.current = new Audio(url);
+  }, [url]);
+
+  useEffect(() => {
+    if (playing) {
+      audio.current.play();
+      return;
+    }
+    audio.current.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audio.current.muted = muted;
+  }, [muted]);
+
+  useEffect(() => {
+    audio.current.addEventListener('ended', () => setPlaying(false));
+    return () => audio.current.removeEventListener('ended', () => setPlaying(false));
+  }, [audio]);
+
+  useEffect(() => {
+    return () => audio.current.pause();
+  }, []);
+
+  const togglePlaying = () => setPlaying(!playing);
+  const toggleMute = () => setMuted(!muted);
+
+  return { playing, muted, togglePlaying, toggleMute };
+};
+
+export default useAudio;
