5af998d7b0d5dad90304899199c95ceab892aee3
parkeunsik <qkrdmstlr3@naver.com>
2021-12-11 21:31:45 +0900

    [refactor] useAudio 업그레이드
    - url을 하나의 상태로 만들어서 관리
      - 관련 useEffect생성
    - changeMusic함수 추가

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -3,20 +3,30 @@ import { useEffect, useRef, useState } from 'react';
 interface UseAudioType {
   playing: boolean;
   muted: boolean;
+  changeMusic: (url: string) => void;
   togglePlaying: () => void;
   toggleMute: () => void;
 }
 
 const useAudio = (url: string): UseAudioType => {
   const audio = useRef<HTMLAudioElement>();
+  const [link, setLink] = useState(url);
   const [playing, setPlaying] = useState(false);
   const [muted, setMuted] = useState(false);
 
   useEffect(() => {
-    audio.current = new Audio(url);
-  }, [url]);
+    if (!audio.current) {
+      audio.current = new Audio(link);
+      return;
+    }
+    audio.current.pause();
+    audio.current = new Audio(link);
+    audio.current.play();
+    setPlaying(true);
+  }, [link]);
 
   useEffect(() => {
+    if (!audio) return;
     if (playing) {
       audio.current.play();
       return;
@@ -25,6 +35,7 @@ const useAudio = (url: string): UseAudioType => {
   }, [playing]);
 
   useEffect(() => {
+    if (!audio) return;
     audio.current.muted = muted;
   }, [muted]);
 
@@ -40,8 +51,9 @@ const useAudio = (url: string): UseAudioType => {
 
   const togglePlaying = () => setPlaying(!playing);
   const toggleMute = () => setMuted(!muted);
+  const changeMusic = (url: string) => setLink(url);
 
-  return { playing, muted, togglePlaying, toggleMute };
+  return { playing, changeMusic, muted, togglePlaying, toggleMute };
 };
 
 export default useAudio;
