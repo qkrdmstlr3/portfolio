47e5663d3db149429b2604abbc8457ec9a04a6d6
parkeunsik <qkrdmstlr3@naver.com>
2021-12-28 21:14:24 +0900

    [feat] MiddleContainer 더미 구현
    - 더미 데이터를 이용해 구현
    - FourthContainer 역할
    - 스크린에 따라 스타일 조정됨

--- src/components/containers/MiddleContainer/index.tsx
+++ src/components/containers/MiddleContainer/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { ScreenType } from '../../templates/main';
 import * as Style from './styled';
 
-function FirstContainer() {
-  return <Style.Container />;
+interface MiddleContainerProps {
+  screen: ScreenType;
 }
 
-export default FirstContainer;
+function MiddleContainer({ screen }: MiddleContainerProps) {
+  return (
+    <Style.Container screen={screen}>
+      <Style.Explanation>개발을 정말 열심히하는</Style.Explanation>
+      <Style.Name>조개소년</Style.Name>
+    </Style.Container>
+  );
+}
+
+export default MiddleContainer;
--- src/components/containers/MiddleContainer/styled.ts
+++ src/components/containers/MiddleContainer/styled.ts
@@ -1,7 +1,30 @@
 import styled from '@emotion/styled';
+import color from '../../../style/color';
+import { ScreenType } from '../../templates/main';
+import { screenSelector } from './style-selector';
 
-export const Container = styled.div`
-  border: 1px solid black;
-  width: 100%;
-  height: 35%;
+interface ContainerProps {
+  screen: ScreenType;
+}
+
+export const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  transition: all 2s;
+  border: 1px solid ${color.black};
+  background-color: ${color.blue};
+  ${(props) => screenSelector[props.screen]};
+`;
+
+export const Explanation = styled.h2`
+  font-size: 2.4rem;
+  color: ${color.white};
+`;
+
+export const Name = styled.h1`
+  font-size: 6.4rem;
+  color: ${color.white};
 `;
