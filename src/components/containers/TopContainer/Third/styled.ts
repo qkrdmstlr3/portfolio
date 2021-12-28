c1a9f1514c884400f56dc8cc3df19379a3cacf10
parkeunsik <qkrdmstlr3@naver.com>
2021-12-29 02:26:15 +0900

    [feat] Third에 style-selector구현
    - 높이에 따른 유동적인 변화를 주기 위해서 추가구현해줌

--- src/components/containers/TopContainer/Third/styled.ts
+++ src/components/containers/TopContainer/Third/styled.ts
@@ -1,30 +1,36 @@
 import styled from '@emotion/styled';
 import color from '../../../../style/color';
+import { ScreenType } from '../../../templates/main/index';
+import { topSelector, bottomSelector } from './style-selector';
+
+interface ScreenProps {
+  screen: ScreenType;
+}
 
 export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
 
-export const Top = styled.div`
-  width: 100%;
-  height: 200px;
+export const Top = styled.div<ScreenProps>`
   display: flex;
   align-items: center;
   justify-content: center;
 
+  transition: all 2s;
   cursor: pointer;
   color: ${color.white};
   background-color: ${color.red};
   border: 1px solid ${color.black};
+  ${(props) => topSelector[props.screen]};
 `;
 
 export const Title = styled.h2`
   font-size: 2.4rem;
 `;
 
-export const Bottom = styled.div`
-  width: 100%;
-  height: calc(100% - 200px);
+export const Bottom = styled.div<ScreenProps>`
+  transition: all 2s;
   border: 1px solid ${color.black};
+  ${(props) => bottomSelector[props.screen]};
 `;
