5786b8153f9547747c9bf7c8569438a45e9a693a
parkeunsik <qkrdmstlr3@naver.com>
2021-12-29 01:44:22 +0900

    [feat] Seventh 부분 구현
    - Seventh 구현 완료

--- src/components/containers/BottomContainer/Seventh/styled.ts
+++ src/components/containers/BottomContainer/Seventh/styled.ts
@@ -1,7 +1,30 @@
 import styled from '@emotion/styled';
+import color from '../../../../style/color';
 
 export const Container = styled.div`
-  border: 1px solid black;
   width: 23%;
   height: 100%;
+`;
+
+export const Top = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+  border: 1px solid black;
+  background-color: ${color.red};
+`;
+
+export const Title = styled.h2`
+  color: ${color.white};
+  font-size: 2.4rem;
+`;
+
+export const Bottom = styled.div`
+  width: 100%;
+  height: 50%;
+  border: 1px solid black;
 `;
