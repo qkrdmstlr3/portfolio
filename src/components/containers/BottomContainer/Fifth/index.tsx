71303e9118853b2abd504d7e7530ce390a28986f
parkeunsik <qkrdmstlr3@naver.com>
2021-12-29 15:32:17 +0900

    [feat] screen이 skill일 경우 Fifth부분 변경
    - screen이 skill상태일 떄 나의 기술 스택 렌더링

--- src/components/containers/BottomContainer/Fifth/index.tsx
+++ src/components/containers/BottomContainer/Fifth/index.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { ScreenType } from '../../../templates/main';
 import * as Style from './styled';
+import SkillBox from '../../../UI/SkillBox';
 
 interface FifthProps {
   screen: ScreenType;
+  changing: boolean;
   changeScreen: (screen: ScreenType) => void;
 }
 
-function Fifth({ screen, changeScreen }: FifthProps) {
+function Fifth({ screen, changing, changeScreen }: FifthProps) {
+  const middleTopSkills = [
+    'https://techstack-generator.vercel.app/js-icon.svg',
+    'https://techstack-generator.vercel.app/ts-icon.svg',
+    'https://techstack-generator.vercel.app/rescript-icon.svg',
+  ];
+  const middleBottomSkills = [
+    'https://techstack-generator.vercel.app/storybook-icon.svg',
+    'https://techstack-generator.vercel.app/jest-icon.svg',
+    'https://techstack-generator.vercel.app/testinglibrary-icon.svg',
+  ];
+
   return (
     <Style.Container>
       <Style.Left screen={screen} onClick={() => changeScreen('experience')}>
         <Style.Title>experience</Style.Title>
       </Style.Left>
       <Style.Middle screen={screen}>
-        <Style.MiddleTop />
-        <Style.MiddleBottom />
+        <Style.MiddleTop>
+          {!changing && screen === 'skill' && <SkillBox skills={middleTopSkills} align="row" />}
+        </Style.MiddleTop>
+        <Style.MiddleBottom>
+          {!changing && screen === 'skill' && <SkillBox skills={middleBottomSkills} align="row" />}
+        </Style.MiddleBottom>
       </Style.Middle>
       <Style.Right screen={screen} />
     </Style.Container>
