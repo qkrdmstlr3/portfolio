3cfd4fbe17c0617ee7e17866cd4bb4a8c9f9285e
parkeunsik <qkrdmstlr3@naver.com>
2021-12-29 19:04:57 +0900

    [feat] react-helmet구현
    - head정보위해서 helmet구현 및 적용

---
+++ src/lib/Helmet.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Helmet as ReactHelmet } from 'react-helmet';
+
+function Helmet() {
+  return (
+    <ReactHelmet title="SHELLBOY">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet" />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        charSet="UTF-8"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+    </ReactHelmet>
+  );
+}
+
+export default Helmet;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainTemplate from '../components/templates/main';
+import Helmet from '../lib/Helmet';
 import GlobalStyle from '../style/GlobalStyle';
 
 function IndexPage() {
   return (
     <>
+      <Helmet />
       <GlobalStyle />
       <MainTemplate />
     </>
