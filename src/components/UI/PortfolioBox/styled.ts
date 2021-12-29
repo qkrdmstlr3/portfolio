45cea4f723afda4002e6bb4acef8981fe47187af
parkeunsik <qkrdmstlr3@naver.com>
2021-12-30 00:54:03 +0900

    [feat] screen이 portfolio일 경우 Fifth변경
    - Fifth의 middleTop에 Carousel 추가
    - PortfolioBox Component를 만들어서 Carousel에 포트폴리오에 대한 간단한 설명을 렌더링할 수 있도록 함

---
+++ src/components/UI/PortfolioBox/styled.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const Date = styled.span`
+  font-size: 2.1rem;
+`;
+
+export const Explanation = styled.h3`
+  font-size: 2.4rem;
+`;
