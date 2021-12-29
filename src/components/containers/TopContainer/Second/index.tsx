a30ed75830774fa957fcf161f5a387c8f00d3f82
parkeunsik <qkrdmstlr3@naver.com>
2021-12-29 15:43:11 +0900

    [feat] screen이 skill일 경우 Second부분 변경
    - screen이 skill상태일 떄 나의 기술 스택 렌더링

--- src/components/containers/TopContainer/Second/index.tsx
+++ src/components/containers/TopContainer/Second/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import * as Style from './styled';
 import { ScreenType } from '../../../templates/main/index';
+import SkillBox from '../../../UI/SkillBox';
 
 interface SecondProps {
   screen: ScreenType;
+  changing: boolean;
   changeScreen: (screen: ScreenType) => void;
 }
 
-function Second({ screen, changeScreen }: SecondProps) {
+function Second({ screen, changing, changeScreen }: SecondProps) {
+  const middleSkills = [
+    'https://techstack-generator.vercel.app/react-icon.svg',
+    'https://techstack-generator.vercel.app/redux-icon.svg',
+    'https://techstack-generator.vercel.app/gatsby-icon.svg',
+  ];
+  const rightSkills = [
+    'https://techstack-generator.vercel.app/sass-icon.svg',
+    'https://techstack-generator.vercel.app/restapi-icon.svg',
+    'https://techstack-generator.vercel.app/github-icon.svg',
+  ];
+
   return (
     <Style.Container screen={screen}>
       <Style.Left>
@@ -16,8 +29,10 @@ function Second({ screen, changeScreen }: SecondProps) {
         </Style.LeftTop>
         <Style.LeftBottom />
       </Style.Left>
-      <Style.Middle />
-      <Style.Right />
+      <Style.Middle>
+        {!changing && screen === 'skill' && <SkillBox skills={middleSkills} align="column" />}
+      </Style.Middle>
+      <Style.Right>{!changing && screen === 'skill' && <SkillBox skills={rightSkills} align="column" />}</Style.Right>
     </Style.Container>
   );
 }
