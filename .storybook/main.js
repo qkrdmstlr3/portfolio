fc3a7c414658dae3e5c787073271307ad9b3e0b2
parkeunsik <qkrdmstlr3@naver.com>
2022-01-03 00:18:20 -0500

    [feat] 스토리북 플러그인 addon-knobs 추가
    - 스토리북의 플러그인인 addon-knobs 추가 구현

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
     'storybook-addon-gatsby',
+    '@storybook/addon-knobs',
   ],
   core: {
     builder: 'webpack5',
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Helmet from '../src/lib/Helmet';
 import GlobalStyle from '../src/style/GlobalStyle';
 
 export const decorators = [
   (Story) => (
     <>
+      <Helmet />
       <Story />
       <GlobalStyle />
     </>
