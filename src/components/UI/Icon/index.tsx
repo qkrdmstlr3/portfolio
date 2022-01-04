b032a32319bb6af6be5dc7fd0299a0412c11cdd9
parkeunsik <qkrdmstlr3@naver.com>
2022-01-04 13:48:19 -0500

    [refactor] add href option to Icon Component
    - 아이콘에 href옵션 추가해서 이동할 수 있도록 함

--- src/components/UI/Icon/index.tsx
+++ src/components/UI/Icon/index.tsx
@@ -7,10 +7,21 @@ interface IconProps {
   icon: IconType;
   color: string;
   size: string;
+  href?: string;
 }
 
-function Icon({ icon, color, size }: IconProps) {
+function Icon({ icon, color, size, href }: IconProps) {
   const SvgIcon = Icons[icon];
+
+  if (href) {
+    return (
+      <Style.Container size={size}>
+        <Style.Href href={href} target="_blank">
+          <SvgIcon color={color} />
+        </Style.Href>
+      </Style.Container>
+    );
+  }
 
   return (
     <Style.Container size={size}>
--- src/components/UI/Icon/styled.ts
+++ src/components/UI/Icon/styled.ts
@@ -12,3 +12,11 @@ export const Container = styled.div<ContainerProps>`
   height: ${(props) => props.size};
   cursor: pointer;
 `;
+
+export const Href = styled.a`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
