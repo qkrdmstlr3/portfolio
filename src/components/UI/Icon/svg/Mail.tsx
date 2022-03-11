d331471afbf42c21c30554bddef805ec7bc60dad
parkeunsik <qkrdmstlr3@naver.com>
2022-03-12 01:26:34 +0900

    [style] svg에 width 부여
    - 아이패드와 아이폰에서는 아이콘이 보여지지않아 width를 부여

--- src/components/UI/Icon/svg/Mail.tsx
+++ src/components/UI/Icon/svg/Mail.tsx
@@ -6,7 +6,7 @@ interface IconProps {
 
 function Mail({ color }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%" height="100%">
       <path
         fill={color}
         d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"
