db0d8c33aecb256ff6b979d843d004eede4e79a5
parkeunsik <qkrdmstlr3@naver.com>
2022-03-20 01:42:32 +0900

    [feat] useScreen hook 생성
    - screen상태관리를 위한 useScreen  hook 생성

---
+++ src/hooks/useScreen.ts
@@ -0,0 +1,38 @@
+import { useRecoilState } from 'recoil';
+import { CHANGE_SCREEN_SECOND } from '../constants/variables';
+import { screenState, ScreenType, ScreenStateType } from '../recoil/screen/atom';
+
+interface UseScreenProps {
+  screenWantedToChange?: ScreenType;
+}
+
+interface UseScreenTypes {
+  screen: ScreenStateType;
+  changeScreen: () => void;
+}
+
+function useScreen({ screenWantedToChange }: UseScreenProps): UseScreenTypes {
+  const [screen, setScreen] = useRecoilState(screenState);
+
+  const changeScreen = () => {
+    if (!screenWantedToChange) return;
+    setScreen({
+      changing: true,
+      currentScreen: screen.currentScreen === screenWantedToChange ? 'main' : screenWantedToChange,
+    });
+
+    setTimeout(() => {
+      setScreen({
+        currentScreen: screen.currentScreen === screenWantedToChange ? 'main' : screenWantedToChange,
+        changing: false,
+      });
+    }, CHANGE_SCREEN_SECOND * 1000);
+  };
+
+  return {
+    screen,
+    changeScreen,
+  };
+}
+
+export default useScreen;
