9924ce81c8e03ee829b2994ba44e64a945dd70bd
parkeunsik <qkrdmstlr3@naver.com>
2022-03-22 20:34:34 +0900

    [refactor] MiddleContainer 리팩토링
    - 중복을 제거

--- src/components/containers/MiddleContainer/index.tsx
+++ src/components/containers/MiddleContainer/index.tsx
@@ -1,41 +1,43 @@
+// Dependencies
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import useScreen from '../../../hooks/useScreen';
 import * as Style from './styled';
+// Hooks
+import useScreen from '../../../hooks/useScreen';
+// Components
 import Carousel from '../../UI/Carousel';
-import CenterBox from '../../UI/CenterBox/index';
-import { experiences, portfolios } from '../../../constants/portfolio';
+// Constants & Types
 import { SLIDER_SECOND } from '../../../constants/variables';
+import { experiences, portfolios } from '../../../constants/portfolio';
 import { carouselIndexState } from '../../../recoil/carousel/atom';
 
 function MiddleContainer() {
   const carouselIndex = useRecoilValue(carouselIndexState);
   const { screen } = useScreen({});
 
+  const getComponent = () => {
+    switch (screen.currentScreen) {
+      case 'experience':
+      case 'portfolio': {
+        const items =
+          screen.currentScreen === 'experience'
+            ? experiences.map((exp, index) => ({ id: index, text: exp.title }))
+            : portfolios.map((exp, index) => ({ id: index, text: exp.title }));
+        return !screen.changing && <Carousel second={SLIDER_SECOND} items={items} carouselIndex={carouselIndex} />;
+      }
+      default:
+        return (
+          <>
+            <Style.Explanation>생각을 현실로 구현하는</Style.Explanation>
+            <Style.Name>조개소년</Style.Name>
+          </>
+        );
+    }
+  };
+
   return (
     <Style.Container aria-label="middle-container" screen={screen.currentScreen}>
-      {(screen.currentScreen === 'main' || screen.currentScreen === 'skill' || screen.currentScreen === 'contact') && (
-        <>
-          <Style.Explanation>생각을 현실로 구현하는</Style.Explanation>
-          <Style.Name>조개소년</Style.Name>
-        </>
-      )}
-      {!screen.changing && screen.currentScreen === 'experience' && (
-        <Carousel
-          second={SLIDER_SECOND}
-          items={experiences.map((exp, index) => ({ id: index, text: exp.title }))}
-          carouselIndex={carouselIndex}
-          Component={CenterBox}
-        />
-      )}
-      {!screen.changing && screen.currentScreen === 'portfolio' && (
-        <Carousel
-          second={SLIDER_SECOND}
-          items={portfolios.map((exp, index) => ({ id: index, text: exp.title }))}
-          carouselIndex={carouselIndex}
-          Component={CenterBox}
-        />
-      )}
+      {getComponent()}
     </Style.Container>
   );
 }
