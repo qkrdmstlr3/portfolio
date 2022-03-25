d66ddbfc5f48922f62d68d8f815f5a9e836b49a8
parkeunsik <qkrdmstlr3@naver.com>
2022-03-25 15:12:19 +0900

    [test] Second Component 테스트 코드 수정
    - test-utils.tsx파일을 이용해서 기존 Second Component의 테스트코드를 수정하였다

--- src/components/containers/TopContainer/Second/Second.test.tsx
+++ src/components/containers/TopContainer/Second/Second.test.tsx
@@ -2,17 +2,25 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '../../../../test-utils';
 import Second from '.';
+import useScreen from '../../../../hooks/useScreen';
+import { ScreenType } from '../../../../recoil/screen/atom';
 
+jest.mock('../../../../hooks/useScreen.ts');
 const changeScreenMock = jest.fn();
+const useScreenMock = useScreen as jest.MockedFunction<typeof useScreen>;
+
+const getScreenObj = (currentScreen: ScreenType, changing: boolean) => ({
+  screen: { currentScreen, changing },
+  changeScreen: changeScreenMock,
+});
 
 describe('Component/Container/TopContainer/Second', () => {
   describe('screen test', () => {
     it('main', () => {
-      const component = render(
-        <Second screen="main" changing={false} carouselIndex={1} changeScreen={changeScreenMock} />
-      );
+      useScreenMock.mockReturnValue(getScreenObj('main', false));
+      const component = render(<Second />);
 
       const container = component.getByLabelText('second-container');
       expect(container).toHaveStyle({
@@ -22,9 +30,8 @@ describe('Component/Container/TopContainer/Second', () => {
     });
 
     it('skill', () => {
-      const component = render(
-        <Second screen="skill" changing={false} carouselIndex={1} changeScreen={changeScreenMock} />
-      );
+      useScreenMock.mockReturnValue(getScreenObj('skill', false));
+      const component = render(<Second />);
 
       const container = component.getByLabelText('second-container');
       expect(container).toHaveStyle({
@@ -34,9 +41,8 @@ describe('Component/Container/TopContainer/Second', () => {
     });
 
     it('contact', () => {
-      const component = render(
-        <Second screen="contact" changing={false} carouselIndex={1} changeScreen={changeScreenMock} />
-      );
+      useScreenMock.mockReturnValue(getScreenObj('contact', false));
+      const component = render(<Second />);
 
       const container = component.getByLabelText('second-container');
       expect(container).toHaveStyle({
@@ -46,9 +52,8 @@ describe('Component/Container/TopContainer/Second', () => {
     });
 
     it('portfolio', () => {
-      const component = render(
-        <Second screen="portfolio" changing={false} carouselIndex={1} changeScreen={changeScreenMock} />
-      );
+      useScreenMock.mockReturnValue(getScreenObj('portfolio', false));
+      const component = render(<Second />);
 
       const container = component.getByLabelText('second-container');
       expect(container).toHaveStyle({
@@ -58,26 +63,19 @@ describe('Component/Container/TopContainer/Second', () => {
     });
 
     it('experience', () => {
-      const component = render(
-        <Second screen="experience" changing={false} carouselIndex={1} changeScreen={changeScreenMock} />
-      );
+      useScreenMock.mockReturnValue(getScreenObj('experience', false));
+      const component = render(<Second />);
 
       const container = component.getByLabelText('second-container');
-      expect(container).toHaveStyle({
-        width: '100%',
-        height: '78%',
-      });
+      expect(container).toHaveStyle({ width: '100%', height: '78%' });
     });
   });
 
   it('change screen', () => {
-    const component = render(
-      <Second screen="experience" changing={false} carouselIndex={1} changeScreen={changeScreenMock} />
-    );
+    const component = render(<Second />);
 
     const leftTop = component.getByLabelText('second-left-top');
     fireEvent.click(leftTop);
     expect(changeScreenMock).toHaveBeenCalledTimes(1);
-    expect(changeScreenMock).toHaveBeenCalledWith('contact');
   });
 });
