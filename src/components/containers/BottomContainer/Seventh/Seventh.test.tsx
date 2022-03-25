a319abfa4b4135801f5780ffee3028b33cbb9ae9
parkeunsik <qkrdmstlr3@naver.com>
2022-03-25 15:50:38 +0900

    [test] Seventh Component 테스트 코드 수정
    - test-utils.tsx파일을 이용해서 기존 Seventh Component의 테스트코드를 수정하였다

--- src/components/containers/BottomContainer/Seventh/Seventh.test.tsx
+++ src/components/containers/BottomContainer/Seventh/Seventh.test.tsx
@@ -4,20 +4,30 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Seventh from '.';
+import useScreen from '../../../../hooks/useScreen';
+import { ScreenType } from '../../../../recoil/screen/atom';
 
+jest.mock('../../../../hooks/useScreen.ts');
 const changeScreenMock = jest.fn();
+const useScreenMock = useScreen as jest.MockedFunction<typeof useScreen>;
+
+const getScreenObj = (currentScreen: ScreenType, changing: boolean) => ({
+  screen: { currentScreen, changing },
+  changeScreen: changeScreenMock,
+});
 
 describe('Component/Container/BottomContainer/Seventh', () => {
   it('rendering test', () => {
-    const component = render(<Seventh changeScreen={changeScreenMock} />);
+    useScreenMock.mockReturnValue(getScreenObj('main', false));
+    const component = render(<Seventh />);
     component.getByText('skill');
   });
 
   it('change screen', () => {
-    const component = render(<Seventh changeScreen={changeScreenMock} />);
+    useScreenMock.mockReturnValue(getScreenObj('main', false));
+    const component = render(<Seventh />);
     const seventhTop = component.getByLabelText('seventh-top');
     fireEvent.click(seventhTop);
     expect(changeScreenMock).toHaveBeenCalledTimes(1);
-    expect(changeScreenMock).toHaveBeenCalledWith('skill');
   });
 });
