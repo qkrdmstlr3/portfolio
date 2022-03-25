c52811afd781c5215f2fab823921a5584e35a255
parkeunsik <qkrdmstlr3@naver.com>
2022-03-25 16:40:28 +0900

    [chore] hook테스트 위한 라이브러리 설치
    - react-hook테스트를 위한 라이브러리 설치

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   setupFiles: ['./setupTests.js'],
